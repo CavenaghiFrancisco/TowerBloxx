1a475c39804a3ab43075a671e70dbcd7489aade0
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2022-04-14 17:23:44 -0300

    Added crane function.

--- Assets/DepartmentSpawn.cs
+++ Assets/DepartmentSpawn.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class DepartmentSpawn : MonoBehaviour
 {
+    [SerializeField] private GameObject departmentPrefab;
+    private MeshRenderer mr;
+    private bool deparmentAlreadySpawned = false;
+
+    private void Awake()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
+
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && !deparmentAlreadySpawned)
+        {
+            deparmentAlreadySpawned = true;
+            Instantiate(departmentPrefab, transform.position, Quaternion.identity);
+            StartCoroutine(DepartmentSpawned());
+        }    
+    }
+
+    private IEnumerator DepartmentSpawned()
+    {
+        mr.enabled = false;
+        yield return new WaitForSeconds(3f);
+        mr.enabled = true;
+        deparmentAlreadySpawned = false;
     }
 
 }
